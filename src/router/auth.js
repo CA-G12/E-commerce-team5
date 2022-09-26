36fcca6631c9edd29ea09d4838ef6ff6f4fecc60
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-26 14:23:13 +0300

    Handled checking token Relates #16 close #16

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { signup } = require('../controllers');
+const { authorization, getLoggedData } = require('../middlewares');
 
 router.post('/signup', signup);
+router.get('/isLogged', authorization, getLoggedData);
 
 module.exports = router;
