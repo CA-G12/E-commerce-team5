830e27688329766a1cfc33be134f54df3120ac13
Mohammed alagha <modyagha7@gmail.com>
2022-09-26 15:37:21 +0300

    fix some bugs realtes #25 close #25

--- src/database/queries/users/checkExistance.js
+++ src/database/queries/users/checkExistance.js
@@ -1,7 +1,7 @@
 const connection = require('../../config/connection');
 
 // eslint-disable-next-line arrow-body-style
-const checkExistance = (value) => {
-  return connection.query(`select * from users where email =$1`, [value]);
+const checkExistance = (key, value) => {
+  return connection.query(`select * from users where ${key} =$1`, [value]);
 };
 module.exports = checkExistance;
--- src/router/cart.js
+++ src/router/cart.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { getCart, addCart, deleteCart } = require('../controllers');
+const { authorization } = require('../middlewares');
 
-router.post('/cart', addCart);
-router.get('/cart/:userId', getCart);
-router.delete('/cart/:productId', deleteCart);
+router.post('/cart', authorization, addCart);
+router.get('/cart/:userId', authorization, getCart);
+router.delete('/cart/:productId', authorization, deleteCart);
 
 module.exports = router;
