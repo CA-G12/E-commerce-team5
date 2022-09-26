68c211e22f4fd8766eedc2d109a624703620792c
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-09-26 14:10:10 +0300

    resovle conflict related #7

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,9 @@
 const { signup } = require('./auth');
 const { login } = require('./auth');
-
-module.exports = { signup, login };
 const getProducts = require('./products/getProducts');
 
 module.exports = {
   signup,
+  login,
   getProducts,
 };
