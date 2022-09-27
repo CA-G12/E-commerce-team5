f92b6676f169fd44e0694e5ae821fa1e7cd99a60
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-09-27 11:46:29 +0300

    create the cart component close #27

---
+++ client/src/Components/Cart/index.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+import '../../global_style.css';
+import './style.css';
+
+function Cart() {
+  return (
+    <main className="wrapper">
+      <section className="content">
+        <div className="cart__item">cart item</div>
+        <div className="cart__item">cart item</div>
+        <div className="cart__item">cart item</div>
+        <div className="cart__item">cart item</div>
+      </section>
+    </main>
+  );
+}
+
+export default Cart;
