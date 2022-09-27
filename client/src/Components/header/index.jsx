d5bb4664de6654b8299f2eb62ee4e4010930565d
AseelL <aseel.sahmood@gmail.com>
2022-09-27 16:03:30 +0300

    fixing more style and components, added fetch

--- client/src/Components/header/index.jsx
+++ client/src/Components/header/index.jsx
@@ -1,17 +1,33 @@
 import { useState, useEffect } from 'react';
 import './style.css';
+import { BsFillCartFill } from 'react-icons/fa';
 
 export default function Header() {
-  const [isLogged, setIslogged] = useState(false);
+  const [isLogged, setIslogged] = useState(true);
+  const [userData, setUserdata] = useState({
+    id: '1',
+    username: 'Lesa',
+    image:
+      'https://images.unsplash.com/photo-1615497001839-b0a0eac3274c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y3V0ZSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  });
+
   useEffect(() => {
     fetch('/api/v1/isLogged')
       .then((data) => {
         data.json();
-        if (data.isToken) {
-          setIslogged(true);
-        } else {
-          setIslogged(false);
-        }
+        setIslogged(true);
+        setUserdata({
+          id: 1,
+          username: 'Lesa',
+          image:
+            'https://img.freepik.com/premium-photo/crossbredeed-cat-wearing-bell-isolated-white_191971-23879.jpg',
+        });
+        console.log(userData.username);
+        // if (data.isToken) {
+        //   setIslogged(true);
+        // } else {
+        //   setIslogged(false);
+        // }
       })
       .catch((err) => {
         console.log(err);
@@ -24,7 +40,25 @@ export default function Header() {
         <h1>Aligrandpa</h1>
       </section>
       <section>
-        <a href="/signin">Sign Out</a>
+        <div className="dropdown">
+          <button className="dropbtn" type="button">
+            {userData.username}
+            <div
+              className="userAvatar"
+              style={{
+                backgroundImage: `url(${userData.image})`,
+              }}
+            >
+              hi
+            </div>
+          </button>
+          <div className="dropdown-content">
+            <a href={`/cart/${userData.id}`}>
+              my Cart <BsFillCartFill />
+            </a>
+            <a href="/logout">Logout</a>
+          </div>
+        </div>
       </section>
     </nav>
   ) : (
