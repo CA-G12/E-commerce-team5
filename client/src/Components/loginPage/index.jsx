50cebc343bdd3634a79d388fb142b81af13110ef
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-28 00:39:56 +0300

    onChange instead of onClick Relates #48 close #48

--- client/src/Components/loginPage/index.jsx
+++ client/src/Components/loginPage/index.jsx
@@ -90,7 +90,7 @@ const index = () => {
                   name="email"
                   id="email"
                   placeholder="email"
-                  onClick={(e) => changeLoginFormData(e)}
+                  onChange={(e) => changeLoginFormData(e)}
                   className={loginError?.name === 'email' ? 'inputError' : ''}
                 />
                 <span className="errorLabel">
@@ -106,7 +106,7 @@ const index = () => {
                   className={
                     loginError?.name === 'password' ? 'inputError' : ''
                   }
-                  onClick={(e) => changeLoginFormData(e)}
+                  onChange={(e) => changeLoginFormData(e)}
                 />
                 <span className="errorLabel">
                   {loginError?.name === 'password' ? loginError.error : ''}
