cf443ab47074565e4897e7455207c5261fd9ed8a
AseelL <aseel.sahmood@gmail.com>
2022-09-27 14:02:39 +0300

    added main styling

---
+++ client/src/Components/header/index.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import './style.css';
+
+export default function Header() {
+  const [isLogged, setIslogged] = useState(false);
+  useEffect(() => {
+    fetch('/api/v1/isLogged')
+      .then((data) => {
+        data.json();
+        if (data.isToken) {
+          setIslogged(true);
+        } else setIslogged(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return isLogged ? (
+    <nav className="header">
+      <section>
+        <h1>Aligrandpa</h1>
+      </section>
+      <section>
+        <a href="/signin">Sign Out</a>
+      </section>
+    </nav>
+  ) : (
+    <nav className="header">
+      <section>
+        <h1>Aligrandpa</h1>
+      </section>
+      <section>
+        <a href="/signin">Sign In</a>
+      </section>
+    </nav>
+  );
+}
