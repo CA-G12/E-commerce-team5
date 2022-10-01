3baa51bf5aca18e881f12427faa4a6dccc21c80b
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-10-01 19:52:56 +0300

    Fixed a bug in the cart items counter after login Relates #105 close #105

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-import { Outlet } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Header from './Components/header';
-import Footer from './Components/footer';
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Header from "./Components/header";
+import Footer from "./Components/footer";
 
 function App() {
   const [userData, setUserdata] = useState({ loggedIn: false, checking: true });
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    const p0 = fetch('/api/v1/isLogged').then((data) => data.json());
-    const p1 = fetch(`/api/v1/cart`).then((res) => res.json());
-    Promise.all([p0, p1]).then((values) => {
-      if (values[0].istoken) {
-        setUserdata({ loggedIn: true, checking: false, ...values[0] });
-        setCart(values[1]);
-      } else {
-        setUserdata({ loggedIn: false, checking: false });
-      }
-    });
+    fetch("/api/v1/isLogged")
+      .then((data) => data.json())
+      .then((res) => {
+        if (res.istoken) {
+          setUserdata({ loggedIn: true, checking: false, ...res });
+        } else {
+          setUserdata({ loggedIn: false, checking: false });
+        }
+      });
   }, []);
+  useEffect(() => {
+    if (userData.loggedIn) {
+      fetch(`/api/v1/cart`)
+        .then((res) => res.json())
+        .then((res) => setCart(res));
+    }
+  }, [userData]);
 
   return (
     <>
       <Header user={userData} cart={cart} />
-      <div className="main" style={{ minHeight: '100vh' }}>
+      <div className="main" style={{ minHeight: "100vh" }}>
         {userData.checking ? (
           <div>loading</div>
         ) : (
